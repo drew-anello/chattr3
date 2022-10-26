1954de38330bacec3e16cf9c90c619fa4905de32
Drew Anello <drew.anello@gmail.com>
2022-10-26 18:22:48 -0400

    adds class and room

--- main_app/views.py
+++ main_app/views.py
@@ -5,9 +5,25 @@ from django.http import HttpResponse
 # Define the home view
 
 
+class Room:  # Note that parens are optional if not inheriting from another class
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+
+
+rooms = [
+    Room('titans', 'room for seir titans'),
+    Room('drew and matt', 'room for friends'),
+]
+
+
 def home(request):
     return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
 
 
 def about(request):
     return render(request, 'about.html')
+
+
+def rooms_index(request):
+    return render(request, 'rooms/index.html', {'rooms': rooms})
