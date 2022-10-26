947ab124cbf0be56d0fc3d6738d8f5642f25683b
Drew Anello <drew.anello@gmail.com>
2022-10-26 18:42:30 -0400

    room model create loads

--- main_app/views.py
+++ main_app/views.py
@@ -2,19 +2,8 @@ from django.shortcuts import render
 
 # Add the following import
 from django.http import HttpResponse
+from .models import Room
 # Define the home view
-
-
-class Room:  # Note that parens are optional if not inheriting from another class
-    def __init__(self, name, description):
-        self.name = name
-        self.description = description
-
-
-rooms = [
-    Room('titans', 'room for seir titans'),
-    Room('drew and matt', 'room for friends'),
-]
 
 
 def home(request):
@@ -26,4 +15,5 @@ def about(request):
 
 
 def rooms_index(request):
+    rooms = Room.objects.all()
     return render(request, 'rooms/index.html', {'rooms': rooms})
