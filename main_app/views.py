4c4d27b124003ddfd3074886b5687b9475e402b6
Drew Anello <drew.anello@gmail.com>
2022-10-26 17:50:15 -0400

    about page route loads

--- main_app/views.py
+++ main_app/views.py
@@ -8,3 +8,7 @@ from django.http import HttpResponse
 
 def home(request):
     return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+
+
+def about(request):
+    return HttpResponse('This is the about page')
