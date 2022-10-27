9657fce2bb0b6506e188049be44e70658ab5038e
Drew Anello <drew.anello@gmail.com>
2022-10-27 10:37:34 -0400

    adds login page

--- main_app/models.py
+++ main_app/models.py
@@ -2,6 +2,7 @@ from email import message
 from unicodedata import name
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -9,6 +10,7 @@ from django.urls import reverse
 class Room(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(max_length=250)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
